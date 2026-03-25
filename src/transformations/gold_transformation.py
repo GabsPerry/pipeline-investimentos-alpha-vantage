17e60ebe3411260bf40dac78d1c5f8448effb563
GabsPerry <gabrielperillo.dev@gmail.com>
2026-03-25 21:42:18 +0000

    feature/logger-and-dataQuality

---
+++ src/transformations/gold_transformation.py
@@ -0,0 +1,88 @@
+# Importando bibliotecas PySpark 
+from pyspark.sql.functions import * 
+from pyspark.sql.types import * 
+from pyspark.sql.window import *
+from src.utils.logger import get_logger 
+from src.data_quality.gold_checks import gold_validation 
+
+logger = get_logger('gold')
+logger.info('Iniciando construção da gold...')
+
+try:
+    df_gold = spark.table('silver.crypto_exchange')
+
+    df_gold = df_gold.withColumn('spread', col('ask_price')-col('bid_price'))\
+        .withColumn('spread_pct', (col('ask_price')-col('bid_price'))/col('exchange_rate'))
+
+    w_price_ystd = Window.partitionBy('crypto_code','converted_code').orderBy('last_refreshed_date')
+
+    df_gold = df_gold.withColumn('price_today', col('exchange_rate'))\
+        .withColumn('price_yesterday', lag('exchange_rate').over(w_price_ystd))\
+        .withColumn('perc_change', (col('price_today')-col('price_yesterday'))/col('price_yesterday'))
+
+    w_priority = Window.partitionBy('crypto_code','converted_code').orderBy(desc('last_refreshed_date'))
+
+    df_gold = df_gold.withColumn('priority', row_number().over(w_priority))
+
+    w_rank_price = Window.orderBy(desc(col('price_today')))
+
+    df_gold = df_gold.withColumn('price_rank', rank().over(w_rank_price))\
+        .withColumn('best_spread_rank', rank().over(Window.orderBy('spread')))\
+        .withColumn('best_spread_pct_rank', rank().over(Window.orderBy('spread_pct')))
+
+    #df_gold.write.mode('overwrite').format('delta').saveAsTable('workspace.gold.crypto_metrics') 
+
+    logger.info('Iniciando Data Quality da gold...')
+    
+    df_gold.createOrReplaceTempView('temp_vw_df_gold')
+
+    df_check = spark.table('temp_vw_df_gold') 
+    gold_validation(df_check) 
+    #df.filter(col('perc_change').isNull()).limit(5).display()
+
+    logger.info('Checagem OK!! Nenhum problema encontrado.')
+    logger.info('Iniciando merge com a gold...')
+
+    spark.sql("""
+        MERGE INTO gold.crypto_metrics as A
+            USING temp_vw_df_gold as B 
+                ON A.crypto_code = B.crypto_code 
+                AND A.converted_code = B.converted_code 
+                AND A.last_refreshed_date = B.last_refreshed_date 
+            WHEN MATCHED THEN 
+            UPDATE SET 
+                A.crypto_name = B.crypto_name 
+                , A.converted_name = B.converted_name 
+                , A.exchange_rate = B.exchange_rate 
+                , A.time_zone = B.time_zone 
+                , A.bid_price = B.bid_price
+                , A.ask_price = B.ask_price 
+                , A.upload_date = B.upload_date 
+                , A.updated_date = current_timestamp() 
+                , A.spread = B.spread 
+                , A.spread_pct = B.spread_pct
+                , A.price_today = b.price_today 
+                , A.price_yesterday = b.price_yesterday 
+                , A.perc_change = b.perc_change 
+                , A.priority = b.priority 
+                , A.price_rank = b.price_rank 
+                , A.best_spread_rank = b.best_spread_rank 
+                , A.best_spread_pct_rank = b.best_spread_pct_rank 
+            WHEN NOT MATCHED 
+            THEN INSERT *
+    """)
+
+    logger.info('Merge da gold executado com sucesso!!')
+    
+    metrics = spark.sql("""
+        DESCRIBE HISTORY gold.crypto_metrics
+    """).select("operationMetrics").first()[0]
+
+    print("Resultado do MERGE:") 
+    print(f"Linhas inseridas: {metrics.get('numTargetRowsInserted', 0)}") 
+    print(f"Linhas atualizadas: {metrics.get('numTargetRowsUpdated', 0)}") 
+
+except Exception as e:
+    logger.exception(f'Erro ao construir a gold: {e}')
+    raise
+ 
