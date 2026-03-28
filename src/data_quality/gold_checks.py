45ec6dbab4986c98bee577fbdce3efa26d8ad56c
Gabriel Perillo <59711908+GabsPerry@users.noreply.github.com>
2026-03-28 20:45:40 -0300

    Enhance gold_validation with data quality checks
    Added validation for empty DataFrame and required columns.

--- src/data_quality/gold_checks.py
+++ src/data_quality/gold_checks.py
@@ -1,9 +1,29 @@
 from pyspark.sql import functions as F
 
 def gold_validation(df):
+    
+    if df.count() == 0:
+        raise Exception('Data Quality Error: Gold truncada')
+
+    required_cols = [
+        'exchange_rate',
+        'spread',
+        'spread_pct',
+        'price_today',
+        'priority',
+        'price_rank',
+        'best_spread_rank',
+        'best_spread_pct_rank'
+    ]
+    
     #if df.filter(F.col('perc_change').isNull()).count() > 0:
     #    raise Exception('Data Quality Error: Gold Percentual Change Nulo')
+    
     if df.filter(F.col('spread') < 0).count() > 0:
         raise Exception('Data Quality Error: Gold Spread Negativo')
+
+    for col in required_cols:
+        if df.filter(F.col(col).isNull()).count() > 0:
+            raise Exception(f'Data Quality Error: Gold métrica {col} nula')
     
     return True
